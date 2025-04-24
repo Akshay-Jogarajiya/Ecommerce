fa67d9562d46b23d8ce3ed8273130e02c5b2f770
Akshay-Jogarajiya <jogarajiyaakshay@gmail.com>
2025-04-24 14:37:50 +0530

    add product image

--- src/main/java/com/akshay/ecommerce/controller/ProductController.java
+++ src/main/java/com/akshay/ecommerce/controller/ProductController.java
@@ -4,9 +4,12 @@ import com.akshay.ecommerce.model.Product;
 import com.akshay.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -28,8 +31,44 @@ public class ProductController {
         Product product = productService.getProductById(id);
 
         if (product != null)
-            return new ResponseEntity<>(product , HttpStatus.OK);
+            return ResponseEntity.ok(product);
         else
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product ,
+                                        @RequestPart MultipartFile imageFile) {
+        try {
+            System.out.println("In add product Controller");
+            Product product1 = productService.addProduct(product , imageFile);
+            return ResponseEntity.ok().body(product1);
+        }catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId) {
+
+        Product product = productService.getProductById(productId);
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(product.getImageType()))
+                .body(product.getImageData());
+    }
+
+    @PutMapping("/product/{productId}")
+    public ResponseEntity<String> updateProduct(@PathVariable int productId ,
+                                                @RequestPart Product product ,
+                                                @RequestPart MultipartFile imageFile) {
+        try {
+            productService.updateProduct(productId , product , imageFile);
+            return ResponseEntity.ok()
+                    .body("product update");
+        } catch (IOException e) {
+            return ResponseEntity.badRequest()
+                    .body("product not updated")
+        }
+
     }
 }
--- src/main/java/com/akshay/ecommerce/model/Product.java
+++ src/main/java/com/akshay/ecommerce/model/Product.java
@@ -1,10 +1,7 @@
 package com.akshay.ecommerce.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,6 +20,32 @@ public class Product {
     private int id;
     private String name;
     private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+
+//    @JsonFormat(shape = JsonFormat.Shape.STRING , pattern = "dd-MM-yyyy")  handle in React
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+
+    @Lob
+    private byte[] imageData;
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public String getImageType() {
+        return imageType;
+    }
+
+    public byte[] getImageData() {
+        return imageData;
+    }
 
     public int getId() {
         return id;
@@ -44,23 +67,67 @@ public class Product {
         return category;
     }
 
+    public String getBrand() {
+        return brand;
+    }
+
+    public boolean isProductAvailable() {
+        return productAvailable;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
     public Date getReleaseDate() {
         return releaseDate;
     }
 
-    public boolean isAvailable() {
-        return available;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    private BigDecimal price;
-    private String category;
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING , pattern = "dd-MM-yyyy")
-    private Date releaseDate;
-    private boolean available;
-    private int quantity;
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public void setProductAvailable(boolean productAvailable) {
+        this.productAvailable = productAvailable;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
+    }
+
+    public void setImageData(byte[] imageData) {
+        this.imageData = imageData;
+    }
 }
